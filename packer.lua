2f62893dc6b1bca5c4932ecca8b33d14006580ed
Joshua D. Boyd <jdboyd@jdboyd.net>
2024-08-23 22:08:51 -0400

    Generate help.

--- packer.lua
+++ packer.lua
@@ -1,3 +1,20 @@
+-- Function to print help information
+local function printHelp()
+    print("Usage: lua script_name.lua [options] file1 file2 ...")
+    print("Options:")
+    print("  --title=TITLE          Sets the title of the project (required)")
+    print("  --author=AUTHOR        Sets the author's name (optional)")
+    print("  --desc=DESCRIPTION     Sets a description of the project (optional)")
+    print("  --site=URL             Sets the website URL of the project (optional)")
+    print("  --license=LICENSE      Sets the license of the project (optional)")
+    print("  --version=VERSION      Sets the version number of the project (optional)")
+    print("  --output=FILEPATH      Specifies the output file path (optional)")
+    print("  --main=FILEPATH        Specifies a main file to be appended without modifications (optional)")
+    print("  --help                 Displays this help message")
+    print("\nExample:")
+    print("  lua script_name.lua --title=\"Project Title\" --author=\"Your Name\" file1.txt file2.txt --output=output.txt")
+end
+
 -- Function to generate metadata header based on command line flags
 local function generateHeader(options)
     local header = ""
@@ -34,9 +51,12 @@ end
 -- Process command line arguments
 local files = {}
 local options = {}
+local helpRequested = false
 
 for i = 1, #arg do
-    if arg[i]:match("^--output=") then
+    if arg[i]:match("^--help$") then
+        helpRequested = true
+    elseif arg[i]:match("^--output=") then
         options.outputFilePath = arg[i]:sub(10)
     elseif arg[i]:match("^--main=") then
         options.mainFilePath = arg[i]:sub(8)
@@ -55,6 +75,11 @@ for i = 1, #arg do
     else
         table.insert(files, arg[i])
     end
+end
+
+if helpRequested then
+    printHelp()
+    return
 end
 
 if not options.title then
